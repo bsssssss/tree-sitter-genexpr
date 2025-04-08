d054baf7216552bae56c68b55dc26e3441ae93cf
Bss <mx.bss@proton.me>
2025-04-08 13:06:04 +0200

    modified queries/tags.scm, 'tree-sitter test' return no errors now

--- queries/tags.scm
+++ queries/tags.scm
@@ -1,1 +1,2 @@
-(function_declarator declarator: (identifier) @name) @definition.function
+;(function_declarator declarator: (identifier) @name) @definition.function
+(function_declaration name: (identifier) @name) @definition.function
