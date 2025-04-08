2254fd91e7ca86855b593bb6ee1b6fd1d14d6076
Bss <mx.bss@proton.me>
2025-04-08 20:40:56 +0200

    Add punctuation highlights, more precise keywords. trying to make function declaration parameters and arguments in function call work

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -1,33 +1,31 @@
 (identifier) @variable
 
+; Function parameters in function declarations
+(function_declaration_parameter
+  (identifier) @variable.parameter)
+
+; Function call arguments
+(call_member_expression
+  arguments: (identifier) @variable.argument)
+
 ; ((identifier) @constant
 ;  (#match? @constant "^[_a-zA-Z][_a-zA-Z0-9]*$"))
 
-"break" @keyword
-"continue" @keyword
-"else" @keyword
-"for" @keyword
-"if" @keyword
-"return" @keyword
-"while" @keyword
+(["(" ")"]) @punctuation.bracket
+(["{" "}"]) @punctuation.bracket
+(["[" "]"]) @punctuation.bracket
+([";" "," "."]) @punctuation.delimiter
 
-; "-" @operator
-; "-=" @operator
-; "->" @operator
-; "=" @operator
-; "!=" @operator
-; "*" @operator
-; "&" @operator
-; "&&" @operator
-; "+" @operator
-; "+=" @operator
-; "<" @operator
-; "==" @operator
-; ">" @operator
-; "||" @operator
-;
-; "." @delimiter
-; ";" @delimiter
+"break" @keyword.break
+"continue" @keyword.continue
+"return" @keyword.return
+
+(["if" "else"]) @keyword.conditional
+(["for" "while"]) @keyword.repeat
+
+"=" @operator
+
+(binary_expression) @operator
 
 (string_literal) @string
 
@@ -39,6 +37,7 @@
 
 (postfix_expression
   object: (identifier) @function)
+
 (function_declaration
   name: (identifier) @function)
 
